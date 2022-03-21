e124e98a51a59f919084cd5d5e75a008a47da92a
Ikenna Okonkwo <ikennaokonkwo2012@gmail.com>
2022-03-20 21:58:00 -0400

    User can compose tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -1,18 +1,19 @@
 package com.codepath.apps.restclienttemplate.models
 
+import android.os.Parcelable
 import com.codepath.apps.restclienttemplate.TimeFormatter
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 import org.json.JSONObject
 
-class Tweet {
-
-    var body: String = ""
-    var createdAt: String = ""
-    var user: User? = null
+@Parcelize
+class Tweet(var body: String = "", var createdAt: String = "", var user: User? = null) :
+    Parcelable {
 
     fun getFormattedTimestamp(): String {
         return TimeFormatter.getTimeDifference(createdAt)
     }
+
     companion object {
         fun fromJson(jsonObject: JSONObject): Tweet {
             val tweet = Tweet()
